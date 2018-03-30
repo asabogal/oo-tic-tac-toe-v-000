a74b42ee7f343d3f9faf199b22e9a34fb06eca65
Alejandro Sabogal <asabogal.git@gmail.com>
2018-03-30 13:25:21 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,6 +60,12 @@ class TicTacToe
   end
 end
 
-
+def won?
+WIN_COMBINATIONS.detect do |win_combo|
+  @board[win_combo[0]] == @board[win_combo[1]] &&
+  @board[win_combo[1]] == @board[win_combo[2]] &&
+  position_taken?(win_combo[0])
+end
+end
 
 end
